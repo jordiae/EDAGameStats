bb669c1b5b70cda7257eb8bd8344dec1a4665463
Jordi Armengol Estapé <me@jordiae.com>
2016-12-05 11:04:35 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,16 +2,7 @@
 g++ -o stats stats.cc
 rm data.txt
 for (( i = 0; i < 100000; i++ )); do
-	./script_default.sh >> data.txt
-    ./fixed1.sh >> data.txt
-    ./fixed2.sh >> data.txt
-    ./fixed3.sh >> data.txt
-    ./fixed4.sh >> data.txt
-    ./fixed5.sh >> data.txt
-    ./fixed6.sh >> data.txt
-    ./generator1.sh >> data.txt
-    ./generator2.sh >> data.txt
-    ./generator3.sh >> data.txt
+    ./script.sh >> data.txt
     ./stats < data.txt
 done
 ./stats < data.txt
