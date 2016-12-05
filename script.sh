f7c56b19ef5ed0420c503f771139e4bf4417d791
Jordi Armengol Estapé <me@jordiae.com>
2016-12-05 11:05:10 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./Game Sovereignv9 Dummy Dummy Dummy -s $RANDOM -i default.cnf -o default.res 2> >(grep -c "Sovereignv9 got top score")
+./Game MyBot Dummy Dummy Dummy -s $RANDOM -i default.cnf -o default.res 2> >(grep -c "MyBot got top score")
